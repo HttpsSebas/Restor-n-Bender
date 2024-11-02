aef2d73f4b5bdfb30a593bb69f7bbeb2ace5e88c
HttpsSebas <130616272+HttpsSebas@users.noreply.github.com>
2024-11-01 21:13:42 -0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -2,7 +2,11 @@ import "./App.css";
 import Home from "./components/home";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div>
+      <Home />
+    </div>
+  );
 }
 
 export default App;
